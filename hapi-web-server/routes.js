fad4b065ac394bccfaa348739edeeff70a2e0b17
dadanmdahlan <dadan.muhammad13@gmail.com>
2021-05-17 13:57:20 +0700

    belajar sampai membuat resfull sederhana menggunakan database array

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -48,7 +48,7 @@ const routes =[
         method:'POST',
         path:'/login',
         handler:(request , h) => {
-            const {username, password} = request.payload;
+            const {username,password} = request.payload;
             return `Welcome ${username}`;
         }
     },
